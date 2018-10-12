5a638bae9fe6510a7bb287aca3e8afd026d1c463
Hannah Mehravari <2249293m@student.gla.ac.uk>
2018-10-13 00:28:28 +0100

    printing matrix now

--- game/board.py
+++ game/board.py
@@ -1,21 +1,21 @@
 import numpy as np
+
+
 class Board:
 
     def __init__(self):
-        cell_array = np.empty((30,12))
-        cell_array[0,0:2] = 0
-        cell_array[5,0:5] = 1
-        cell_array[7,0:3] = 1
-        cell_array[11,0:5] = 0
-        cell_array[12,0:5] = 1
-        cell_array[16,0:3] = 0
-        cell_array[18,0:5] = 0
-        cell_array[23,0:2] = 1
-
+        self.cell_array = np.zeros((30, 12))
+        self.cell_array[0, 0:2] = 2
+        self.cell_array[5, 0:5] = 1
+        self.cell_array[7, 0:3] = 1
+        self.cell_array[11, 0:5] = 2
+        self.cell_array[12, 0:5] = 1
+        self.cell_array[16, 0:3] = 2
+        self.cell_array[18, 0:5] = 2
+        self.cell_array[23, 0:2] = 1
 
     def get_cell_array(self):
-        print(self.cell_array)
-
+        return self.cell_array
 
         # cell_list = [[0, 0],
         #              [],
@@ -43,11 +43,6 @@ class Board:
         #              [1, 1],
         #              ]
 
-b = Board()
-b.get_cell_array()
-
-
-
-
-
-
+if __name__ == '__main__':
+    b = Board()
+    print(b.get_cell_array())

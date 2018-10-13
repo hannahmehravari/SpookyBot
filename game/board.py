e1b2646633643e8afbd944839c88e22ece39d4a0
Hannah Mehravari <2249293m@student.gla.ac.uk>
2018-10-13 01:15:53 +0100

    board prints perfectly but needs fancy formatting

--- game/board.py
+++ game/board.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class Board:
 
     def __init__(self):
-        self.cell_array = np.empty((30, 12))
+        self.cell_array = np.empty((24, 15))
         self.cell_array[:, :] = 0
         self.cell_array[0, 0:2] = 1
         self.cell_array[5, 0:5] = 2
@@ -13,9 +14,18 @@ class Board:
         self.cell_array[16, 0:3] = 1
         self.cell_array[18, 0:5] = 1
         self.cell_array[23, 0:2] = 2
+        self.cell_array = self.cell_array.swapaxes(0, 1)
 
-    def get_cell_array(self):
-        return self.cell_array
+    def draw_board(self):
+        bottom_half = self.cell_array[:, 0:12]
+        top_half = self.cell_array[:, 12:24]
+        bottom_half = np.flip(bottom_half)
+
+        board = np.concatenate((top_half, bottom_half))
+
+        board = board[~np.all(board == 0, axis=1)]
+
+        return board
 
         # cell_list = [[0, 0],
         #              [],
@@ -46,4 +56,4 @@ class Board:
 
 if __name__ == '__main__':
     b = Board()
-    np.savetxt('bg.txt', b.get_cell_array(), fmt="%4d")
+    np.savetxt('bg.txt', b.draw_board(), fmt="%4d")

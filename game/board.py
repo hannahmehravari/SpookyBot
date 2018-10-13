f333ff5f6a2bda121b558c25369da05e0a6c775e
Hannah Mehravari <2249293m@student.gla.ac.uk>
2018-10-13 01:50:53 +0100

    some fancy formatting added

--- game/board.py
+++ game/board.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sys
 
 class Board:
 
@@ -14,17 +14,17 @@ class Board:
         self.cell_array[16, 0:3] = 1
         self.cell_array[18, 0:5] = 1
         self.cell_array[23, 0:2] = 2
-        self.cell_array = self.cell_array.swapaxes(0, 1)
 
     def draw_board(self):
-        bottom_half = self.cell_array[:, 0:12]
-        top_half = self.cell_array[:, 12:24]
+        full_board = self.cell_array.swapaxes(0, 1)
+        bottom_half = full_board[:, 0:12]
+        top_half = full_board[:, 12:24]
         bottom_half = np.flip(bottom_half)
-
-        board = np.concatenate((top_half, bottom_half))
-
+        separator_row = np.empty((1, 12))
+        separator_row[:, :] = 3
+        board = np.concatenate((top_half, separator_row, separator_row, bottom_half))
         board = board[~np.all(board == 0, axis=1)]
-
+        board[board == 3] = 0
         return board
 
         # cell_list = [[0, 0],
@@ -56,4 +56,7 @@ class Board:
 
 if __name__ == '__main__':
     b = Board()
-    np.savetxt('bg.txt', b.draw_board(), fmt="%4d")
+    h = "11---10----9----8----7----6----5----4----3----2----1----0"
+    f = "12---13---14---15---16---17---18---19---20---21---22---23"
+    np.savetxt(sys.stdout, b.draw_board(), fmt="%4d", header=h, footer=f, comments="  ")
+

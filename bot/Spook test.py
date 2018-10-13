5a65b0e41c787b7ba96332d83b0e5e38139a2b55
Abubakr Mahmood <2257419m@student.gla.ac.uk>
2018-10-13 15:37:00 +0100

    Added more status in seperate test file for bot

--- bot/Spook test.py
+++ bot/Spook test.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Bot
+from discord import Game
 import random
 
 BOT_PREFIX = ("?","!")
@@ -8,18 +9,25 @@ TOKEN = "NTAwNDY0MTgxNDY4NzI1MjU2.DqNjSg.uS1w8xjdvqXKIBzfM-6vilypUC0"
 client= Bot(command_prefix=BOT_PREFIX)
 
 @client.command(
-        name='test'
+        name='backgammon',
         # add details to help function for bot
         description = "Big spook plays backgammon against you",
-        brief = "Big Spook vs you @ Backgammon"
+        brief = "Big Spook vs you @ Backgammon",
+        pass_context = True
     )
-async def backgammon_test():
+async def backgammon_test(context):
     possible_responses = [
         '{0.author.mention}, are you ready to face the big Spook?!',
         'Doot Doot',
         "Cam's hands are italic",
     ]
-    await client.say(random.choice(possible_responses))
+    await client.say(random.choice(possible_responses) +
+                     ", " + context.message.author.mention)
 
     
+@client.event
+async def on_ready():
+    await client.change_presence(game = Game(name='with italic hands'))
+    print ("Logged in as " + client.user.name)
+
 client.run(TOKEN)

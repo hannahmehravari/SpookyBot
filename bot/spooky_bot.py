f485a9c176f50030a3d1314c743d1ee9ca892fe3
Abubakr Mahmood <2257419m@student.gla.ac.uk>
2018-10-13 22:11:43 +0100

    Added all bot commands to main bot file

--- bot/spooky_bot.py
+++ bot/spooky_bot.py
@@ -1,20 +1,23 @@
-# all logic will go here
+# all bot logic will go here
 
 import discord, re
+import random
+from discord.ext.commands import Bot
+from discord import Game
 
-# Creates new client
-client = discord.Client()
+TOKEN = 'NTAwNDY0MTgxNDY4NzI1MjU2.DqNjSg.uS1w8xjdvqXKIBzfM-6vilypUC0'
+client= discord.Client()
+   
+@client.event
+async def on_ready():
+    await client.change_presence(game = Game(name='Backgammon'))
+    print ("Logged in as " + client.user.name)
 
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
-    # 
-    if message.content.startswith('!backgammon'):
-        msg = '{0.author.mention}, are you ready to face the big Spook?!'.format(message)
-        await client.send_message(message.channel, msg)
 
     #dadbot command (detects string "im/i am X" and replies with "hi X, im Dad!")
     if ("im" or "i'm")in message.content.lower():
@@ -23,12 +26,28 @@ async def on_message(message):
         msg = ("Hi "+reply+", I'm Dad!").format(message)
         await client.send_message(message.channel, msg)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    
-# 
-client.run('NTAwNDY0MTgxNDY4NzI1MjU2.DqNjSg.uS1w8xjdvqXKIBzfM-6vilypUC0')
+    # Should be changed to command to activate game
+    # Currently gives random responses from list when acted upon
+    if message.content.startswith('!backgammon'):
+        possible_responses = [
+            '{0.author.mention}, are you ready to face the big Spook?!',
+            'Doot Doot',
+            "You really want to do this, {0.author.mention}",
+        ]
+        await client.send_message(message.channel,
+                                  random.choice(possible_responses).format(message))
+
+    # REAL YOUTUBE LINK TO REAL SPOOK STUFF
+    if message.content.startswith('!spook'):
+        url = "https://www.youtube.com/watch?v=9DECYte0kZ4"
+        await client.send_message(message.channel, url)   
+
+    # Show list of commands to user
+    if message.content.startswith('!help'):  
+        msg = """The currently available commands are: \n
+            !help - shows list of all commands \n
+            !backgammon - starts game of backgammon against spooky bot \n
+            !spook - real scary stuff"""
+        await client.send_message(message.channel, msg)
+        
+client.run(TOKEN)
